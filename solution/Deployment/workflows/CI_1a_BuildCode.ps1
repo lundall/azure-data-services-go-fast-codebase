e9e332b20c43cbd8fa8db70bd3de22b3e40adfa5
Stephen Lundall <Stephen.Lundall@insight.com>
2021-05-12 12:39:35 +0800

    Initial update of config

--- solution/Deployment/workflows/CI_1a_BuildCode.ps1
+++ solution/Deployment/workflows/CI_1a_BuildCode.ps1
@@ -9,12 +9,11 @@ ParseEnvFile("$env:ENVIRONMENT_NAME")
 if (([Environment]::GetEnvironmentVariable("AdsOpts_CI_Enable")) -eq "True")
 {
     Write-Host "Starting CI.."
-    
+    Invoke-Expression -Command  ".\Steps\CI_BuildAdsGoFastDatabase.ps1"
+
     Invoke-Expression -Command  ".\Steps\CI_BuildFunctionApp.ps1"
 
     Invoke-Expression -Command  ".\Steps\CI_BuildWebApp.ps1"
 
-    Invoke-Expression -Command  ".\Steps\CI_BuildAdsGoFastDatabase.ps1"
-    
     Write-Host "Finishing CI.."
 }
